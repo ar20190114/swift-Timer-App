f8338ea0767dcaed9f90a0faae6cde41a0d23efd
ar20190114 <skdr0724@gmail.com>
2023-01-18 15:05:26 +0900

    Initial Commit

---
+++ MyTimerApp/MyTimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerAppApp.swift
+//  MyTimerApp
+//
+//  Created by ryotaban on 2023/01/18.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

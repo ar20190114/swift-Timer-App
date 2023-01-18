5e2a4ca7d94579a62c29f2218aadf02b85c2ef89
ar20190114 <skdr0724@gmail.com>
2023-01-18 15:49:11 +0900

    create button

--- MyTimerApp/ContentView.swift
+++ MyTimerApp/ContentView.swift
@@ -10,8 +10,40 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView{
-            VStack {
-                Text("タイマー画面")
+            ZStack {
+                Image("backgroundTimer")
+                    .resizable()
+                    .ignoresSafeArea()
+                    .aspectRatio(contentMode: .fill)
+                
+                VStack(spacing: 30) {
+                    Text("残り10秒")
+                        .font(.largeTitle)
+                    
+                    HStack {
+                        Button(action: {
+//
+                        }, label: {
+                            Text("スタート")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(width: 140, height: 140)
+                                .background(Color("startColor"))
+                                .clipShape(Circle())
+                        })
+                        
+                        Button(action: {
+//
+                        }, label: {
+                            Text("ストップ")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(width: 140, height: 140)
+                                .background(Color("stopColor"))
+                                .clipShape(Circle())
+                        })
+                    }
+                }
             }
             .toolbar{
                 ToolbarItem(placement: .navigationBarTrailing) {
